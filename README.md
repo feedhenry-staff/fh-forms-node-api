6f8f5d2264b03f0f34515281d4e02892b7b61c75
Colum Bennett <colum.bennett@feedhenry.com>
2014-10-02 10:37:15 +0100

    updated logger dependency, README

--- README.md
+++ README.md
@@ -2,3 +2,31 @@ fh-forms-node-api
 ====
 
 Module contains functons wrapping all $fh.forms node api's for ease of use in future projects.
+
+####How to install
+
+```shell
+npm install git+https://github.com/feedhenry-staff/fh-forms-node-api.git --save
+```
+
+####How to use?
+
+Each function takes 2 parameters, fiirst param is an object of properties for given API and second param is a callback function
+to handle responce from request. For more information on $fh.forms API visit the Docs section.
+
+```javascript
+// require module
+var forms = require('fh-forms-node-api');
+
+// expose the functions as endpoints
+// to call using $fh.act or $fh.cloud
+exports.getForm = forms.getForm;
+exports.getForms = forms.getForms;
+exports.getTheme = forms.getTheme;
+exports.getAppClientConfig = forms.getAppClientConfig;
+exports.submitFormData = forms.submitFormData;
+exports.getSubmissionStatus = forms.getSubmissionStatus;
+exports.submitFormFile = forms.submitFormFile;
+exports.completeSubmission = forms.completeSubmission;
+```
+
--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-module.exports = function () {
+module.exports = function() {
 
     'use strict';
 
     var fh = require('fh-mbaas-api');
-    var logger = require('winston');
-    var date = new Date().toUTCString();
+    var logger = require('logger')();
 
     // get all forms
-    this.getForms = function (params, cb) {
-        logger.log('info', date + ' :: fh forms API :: getForms endpoint request');
+    this.getForms = function(params, cb) {
+        logger('info', ':: fh forms API :: getForms endpoint request');
 
-        fh.forms.getForms({}, function (err, data) {
+        fh.forms.getForms({}, function(err, data) {
             if (err) {
-                logger.error(date + ' :: fh forms API :: getForms endpoint' + JSON.stringify(err, null, 2));
+                logger.error(' :: fh forms API :: getForms endpoint' + JSON.stringify(err, null, 2));
                 return cb(err, null);
             } else {
                 return cb(null, data);
@@ -21,34 +20,34 @@ module.exports = function () {
     };
 
     // get a form
-    this.getForm = function (params, cb) {
-        logger.log('info', date + ' :: fh forms API :: getForm endpoint request');
+    this.getForm = function(params, cb) {
+        logger('info', ' :: fh forms API :: getForm endpoint request');
 
         if (params && params.formId) {
             fh.forms.getForm({
                 '_id': params.formId
-            }, function (err, data) {
+            }, function(err, data) {
                 if (err) {
-                    logger.log('error', date + ' :: fh forms API :: getForms endpoint' + JSON.stringify(err, null, 2));
+                    logger('error', ' :: fh forms API :: getForms endpoint' + JSON.stringify(err, null, 2));
                     return cb(err, null);
                 } else {
                     return cb(null, data);
                 }
             });
         } else {
-            var err = new Error(date + ' :: fh forms API :: getForms endpoint : formId is not defined');
-            logger.log('error', err);
+            var err = new Error(' :: fh forms API :: getForms endpoint : formId is not defined');
+            logger('error', err);
             return cb(err, null);
         }
     };
 
     // getTheme for form
-    this.getTheme = function (params, cb) {
-        logger.log('info', date + ' :: fh forms API :: getTheme endpoint request');
+    this.getTheme = function(params, cb) {
+        logger('info', ' :: fh forms API :: getTheme endpoint request');
 
-        fh.forms.getTheme({}, function (err, data) {
+        fh.forms.getTheme({}, function(err, data) {
             if (err) {
-                logger.log('error', date + ' :: fh forms API :: getTheme endpoint' + JSON.stringify(err, null, 2));
+                logger('error', ' :: fh forms API :: getTheme endpoint' + JSON.stringify(err, null, 2));
                 return cb(err, null);
             } else {
                 return cb(null, data);
@@ -57,111 +56,111 @@ module.exports = function () {
     };
 
     // get client config
-    this.getAppClientConfig = function (params, cb) {
-        logger.log('info', date + ' :: fh forms API :: getAppClientConfig endpoint request');
+    this.getAppClientConfig = function(params, cb) {
+        logger('info', ' :: fh forms API :: getAppClientConfig endpoint request');
 
         if (params && params.appId) {
             fh.forms.getAppClientConfig({
                 '_id': params.appId
-            }, function (err, data) {
+            }, function(err, data) {
                 if (err) {
-                    logger.log('error', date + ' :: fh forms API :: getAppClientConfig endpoint' + JSON.stringify(err, null, 2));
+                    logger('error', ' :: fh forms API :: getAppClientConfig endpoint' + JSON.stringify(err, null, 2));
                     return cb(err, null);
                 } else {
                     return cb(null, data);
                 }
             });
         } else {
-            var err = new Error(date + ' :: fh forms API :: getAppClientConfig endpoint : params.appId is not defined');
-            logger.log('error', err);
+            var err = new Error(' :: fh forms API :: getAppClientConfig endpoint : params.appId is not defined');
+            logger('error', err);
             return cb(err, null);
         }
     };
 
     // get submit Form Data
-    this.submitFormData = function (params, cb) {
-        logger.log('info', date + ' :: fh forms API :: submitFormData endpoint request');
+    this.submitFormData = function(params, cb) {
+        logger('info', ' :: fh forms API :: submitFormData endpoint request');
 
         if (params && params.submission) {
             fh.forms.submitFormData({
                 'submission': params.submission
-            }, function (err, data) {
+            }, function(err, data) {
                 if (err) {
-                    logger.log('error', date + ' :: fh forms API :: submitFormData endpoint' + JSON.stringify(err, null, 2));
+                    logger('error', ' :: fh forms API :: submitFormData endpoint' + JSON.stringify(err, null, 2));
                     return cb(err, null);
                 } else {
                     return cb(null, data);
                 }
             });
         } else {
-            var err = new Error(date + ' :: fh forms API :: submitFormData endpoint : params.submission is not defined');
-            logger.log('error', err);
+            var err = new Error(' :: fh forms API :: submitFormData endpoint : params.submission is not defined');
+            logger('error', err);
             return cb(err, null);
         }
     };
 
     // get submit Form Data
-    this.getSubmissionStatus = function (params, cb) {
-        logger.log('info', date + ' :: fh forms API :: getSubmissionStatus endpoint request');
+    this.getSubmissionStatus = function(params, cb) {
+        logger('info', ' :: fh forms API :: getSubmissionStatus endpoint request');
 
         if (params && params.submission) {
             fh.forms.getSubmissionStatus({
                 'submission': params.submission
-            }, function (err, data) {
+            }, function(err, data) {
                 if (err) {
-                    logger.log('error', date + ' :: fh forms API :: getSubmissionStatus endpoint' + JSON.stringify(err, null, 2));
+                    logger('error', ' :: fh forms API :: getSubmissionStatus endpoint' + JSON.stringify(err, null, 2));
                     return cb(err, null);
                 } else {
                     return cb(null, data);
                 }
             });
         } else {
-            var err = new Error(date + ' :: fh forms API :: getSubmissionStatus endpoint : params.submission is not defined');
-            logger.log('error', err);
+            var err = new Error(' :: fh forms API :: getSubmissionStatus endpoint : params.submission is not defined');
+            logger('error', err);
             return cb(err, null);
         }
     };
 
     // get submit Form Data
-    this.submitFormFile = function (params, cb) {
-        logger.log('info', date + ' :: fh forms API :: submitFormFile endpoint request');
+    this.submitFormFile = function(params, cb) {
+        logger('info', ' :: fh forms API :: submitFormFile endpoint request');
 
         if (params && params.submission) {
             fh.forms.submitFormFile({
                 'submission': params.submission
-            }, function (err, data) {
+            }, function(err, data) {
                 if (err) {
-                    logger.log('error', date + ' :: fh forms API :: submitFormFile endpoint' + JSON.stringify(err, null, 2));
+                    logger('error', ' :: fh forms API :: submitFormFile endpoint' + JSON.stringify(err, null, 2));
                     return cb(err, null);
                 } else {
                     return cb(null, data);
                 }
             });
         } else {
-            var err = new Error(date + ' :: fh forms API :: submitFormFile endpoint : params.submission is not defined');
-            logger.log('error', err);
+            var err = new Error(' :: fh forms API :: submitFormFile endpoint : params.submission is not defined');
+            logger('error', err);
             return cb(err, null);
         }
     };
 
     // get submit Form Data
-    this.completeSubmission = function (params, cb) {
-        logger.log('info', date + ' :: fh forms API :: completeSubmission endpoint request');
+    this.completeSubmission = function(params, cb) {
+        logger('info', ' :: fh forms API :: completeSubmission endpoint request');
 
         if (params && params.submission) {
             fh.forms.completeSubmission({
                 'submission': params.submission
-            }, function (err, data) {
+            }, function(err, data) {
                 if (err) {
-                    logger.log('error', date + ' :: fh forms API :: completeSubmission endpoint' + JSON.stringify(err, null, 2));
+                    logger('error', ' :: fh forms API :: completeSubmission endpoint' + JSON.stringify(err, null, 2));
                     return cb(err, null);
                 } else {
                     return cb(null, data);
                 }
             });
         } else {
-            var err = new Error(date + ' :: fh forms API :: completeSubmission endpoint : params.submission is not defined');
-            logger.log('error', err);
+            var err = new Error(' :: fh forms API :: completeSubmission endpoint : params.submission is not defined');
+            logger('error', err);
             return cb(err, null);
         }
     };
